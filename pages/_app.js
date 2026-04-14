0c0c0a383e9407a3738a1a2210f6b71f294d3797
Tkveil <kunkels2007@yahoo.com>
2026-04-13 22:38:56 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((reg) => {
+          console.log('Mara SW registered:', reg.scope);
+        })
+        .catch((err) => {
+          console.log('Mara SW failed:', err);
+        });
+    }
+  }, []);
+
   return <Component {...pageProps} />
 }
